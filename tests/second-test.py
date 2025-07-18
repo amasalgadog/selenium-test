20b7bc06649f0544095d5731ed071c0f080641c6
gato4d <alvaro.asalgado@ug.uchile.cl>
2025-07-17 23:18:28 -0400

    fixes for 2nd test, python and msedge driver

--- tests/second-test.py
+++ tests/second-test.py
@@ -1,23 +1,12 @@
-import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.edge.options import Options
-import time
 
-class DuckDuckGoTest(unittest.TestCase):
-    def test_search(self):
-        driver = webdriver.Edge()
-        driver.get("https://duckduckgo.com/")
-        
-        browser = driver.find_element(By.NAME, "q")
-        browser.send_keys("Selenium Python")
-        browser.submit()
-        
-        time.sleep(2)
-        
-        results = driver.find_elements(By.CSS_SELECTOR, "h2")
-        self.assertTrue(len(results) > 0, "No se encontraron resultados")
-        driver.quit()
+def test_search():
+    driver = webdriver.Edge()
+    driver.get("https://github.com")
+    
+    titleElement = driver.find_element(By.ID, "hero-section-brand-heading")
 
-if __name__ == "__main__":
-    unittest.main()
+    assert titleElement.text == "Build and ship software on a single, collaborative platform"
+    
+    driver.quit()
